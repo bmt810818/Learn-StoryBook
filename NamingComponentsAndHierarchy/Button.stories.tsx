e522e146cb21b85a0fb87fa82aaba09905881613
Bui Minh Toan <toan.bui@nexpando.com>
2024-06-04 21:06:24 +0700

    Add Naming components and hierarchy

---
+++ NamingComponentsAndHierarchy/Button.stories.tsx
@@ -0,0 +1,10 @@
+import type { Meta } from '@storybook/react'
+import { Button } from './Button'
+
+// Naming stories
+const meta: Meta<typeof Button> = {
+    title: 'Button',
+    component: Button,
+};
+
+export default meta;
