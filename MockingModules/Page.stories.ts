e8f4da10a672c93ddb49d9df46307921d547e255
Bui Minh Toan <toan.bui@nexpando.com>
2024-06-07 15:27:45 +0700

    Add Setting up and cleaning up

--- MockingModules/Page.stories.ts
+++ MockingModules/Page.stories.ts
@@ -1,10 +1,19 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import MockDate from 'mockdate'
 
 import { getUserFromSession } from '#api/session.mock'
 import { Page } from './Page'
 
 const meta: Meta<typeof Page> = {
   component: Page,
+  // Set the value of Date for every story in the file
+  async beforeEach() {
+    MockDate.set('2024-06-01')
+
+    return () => {
+      MockDate.reset();
+    }
+  }
 }
 
 export default meta
@@ -12,7 +21,7 @@ export default meta
 type Story = StoryObj<typeof Page>
 
 export const Default: Story = {
-  async beforeEach() {
-    getUserFromSession.mockReturnValue({ id: '1', name: 'Alice' })
-  },
+  async play({ canvasElement }) {
+    
+  }
 }
